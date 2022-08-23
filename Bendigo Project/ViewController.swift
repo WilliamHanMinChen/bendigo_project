fbf5f1bc5764816a491ea0c2c79a0db2b9d4254c
HanMin Chen <hche0162@student.monash.edu>
2022-08-23 15:24:27 +1000

    Initial Commit

---
+++ Bendigo Project/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Bendigo Project
+//
+//  Created by William Chen on 2022/8/23.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
